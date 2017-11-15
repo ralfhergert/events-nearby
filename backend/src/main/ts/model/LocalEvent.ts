566cb8b5d3ac0c57b09b81d177bc5780c0c6ba5c
Ralf Hergert <ralf.hergert@gmx.de>
2017-11-15 17:35:27 +0100

    added getter and setter for all attributes

--- backend/src/main/ts/model/LocalEvent.ts
+++ backend/src/main/ts/model/LocalEvent.ts
@@ -2,10 +2,10 @@ export class LocalEvent {
 	private _id: string;
 	private _title: any; // use Map<string,string> after switching to ES6;
 	private _description: any; // use Map<string,string> after switching to ES6;
-	private image: URL;
-	private location: Location;
-	private startTime: Date;
-	private endTime: Date;
+	private _image: URL;
+	private _location: Location;
+	private _startDate: Date;
+	private _endDate: Date;
 
 	get id(): string {
 		return this._id;
@@ -15,15 +15,13 @@ export class LocalEvent {
 		this._id = value;
 	}
 
-
 	get title(): any {
 		return this._title;
 	}
 
 	set title(value: any) {
-		this._title=value;
+		this._title = value;
 	}
-
 
 	get description(): any {
 		return this._description;
@@ -31,5 +29,37 @@ export class LocalEvent {
 
 	set description(value: any) {
 		this._description = value;
+	}
+
+	get image(): URL {
+		return this._image;
+	}
+
+	set image(value: URL) {
+		this._image = value;
+	}
+
+	get location(): Location {
+		return this._location;
+	}
+
+	set location(value: Location) {
+		this._location = value;
+	}
+
+	get startDate(): Date {
+		return this._startDate;
+	}
+
+	set startDate(value: Date) {
+		this._startDate = value;
+	}
+
+	get endDate(): Date {
+		return this._endDate;
+	}
+
+	set endDate(value: Date) {
+		this._endDate = value;
 	}
 }
