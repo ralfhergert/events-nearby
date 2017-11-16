d0f0dda4310da88b117232933891f6fee15d2100
Ralf Hergert <ralf.hergert@gmx.de>
2017-11-16 15:48:05 +0100

    added listeners to synchronize the different language selectors

--- backend/src/main/ts/widget/LanguageSelectorWidget.ts
+++ backend/src/main/ts/widget/LanguageSelectorWidget.ts
@@ -4,18 +4,36 @@ export class LanguageSelectorWidget {
 	readonly $target: any;
 	static languages = new Languages();
 
-	constructor(name: string) {
+	constructor(name: string, disabledLanguages: Array<string>, selectionCandidate: string) {
 		this.$target = jQuery('<select class="languageSelector">').attr('name', name);
 		// render all options.
 		for (let language in LanguageSelectorWidget.languages) {
 			jQuery('<option>')
 				.attr('value', language)
 				.text(LanguageSelectorWidget.languages[language])
+				.attr('disabled', disabledLanguages.indexOf(language) > -1 ? 'disabled' : null)
 				.appendTo(this.$target);
+		}
+		if (disabledLanguages.indexOf(selectionCandidate) < 0) {
+			this.$target.val(selectionCandidate);
 		}
 	}
 
 	public value(): string {
 		return this.$target.val();
+	}
+
+	public updateDisabledLanguages(disabledLanguages: Array<string>) {
+		// remove disabled-state from all options which no longer belong to the disabled languages.
+		this.$target.find('[disabled]').each(function() {
+			jQuery(this).attr('disabled', disabledLanguages.indexOf(jQuery(this).attr('value')) > -1 ? 'disabled' : null)
+		});
+		// make sure all option are disable which should be disabled. - do not disable them if they are the currently selected value.
+		let currentSelection = this.$target.val();
+		for (let language of disabledLanguages) {
+			if (language !== currentSelection) {
+				this.$target.children('option[value="' + language + '"]').attr('disabled', 'disabled');
+			}
+		}
 	}
 }
--- backend/src/main/ts/widget/LocalizableTextListWidget.ts
+++ backend/src/main/ts/widget/LocalizableTextListWidget.ts
@@ -29,15 +29,28 @@ export class LocalizableTextListWidget {
 
 	public createFurtherTextWidget(): void {
 		let thisObj = this;
-		let widget = new LocalizableTextWidget(this.type, this.name + '.' + this.index++);
+		let widget = new LocalizableTextWidget(this.type, this.name + '.' + this.index++, this.allSelectedLanguages());
 		this.textWidgets.push(widget);
 		this.$list.append(widget.$target);
+		// register a change listener for language changes.
+		widget.addLanguageSelectionChangedListener({
+			changed() {
+				thisObj.updateSelectableLanguages();
+			}
+		});
 		// register a widget destroyed listener to remove it from the list again.
 		widget.addWidgetDestroyListener({
 			destroyed(widget: LocalizableTextWidget) {
 				thisObj.textWidgets.splice(thisObj.textWidgets.indexOf(widget), 1);
+				thisObj.updateSelectableLanguages();
 			}
 		});
+		thisObj.updateSelectableLanguages();
+	}
+
+	protected updateSelectableLanguages(): void {
+		let selectedLanguages = this.allSelectedLanguages();
+		this.textWidgets.forEach(widget => { widget.updateDisabledLanguages(selectedLanguages) });
 	}
 
 	public getValues(): any {
@@ -51,6 +64,15 @@ export class LocalizableTextListWidget {
 		});
 		return values;
 	}
+
+	protected allSelectedLanguages(): Array<string> {
+		let selectedLanguages: Array<string> = [];
+		this.textWidgets.forEach(widget =>  {
+			selectedLanguages.push(widget.locale());
+		});
+		return selectedLanguages;
+	}
+
 }
 
 /**
@@ -60,15 +82,19 @@ export class LocalizableTextWidget {
 	readonly $target: any; // should be a jQuery node.
 	readonly name: string;
 	readonly languageSelector: LanguageSelectorWidget;
+	private languageSelectionChangedListener: Array<LanguageSelectionChangedListener> = [];
 	private destroyListeners: Array<WidgetDestroyedListener<LocalizableTextWidget>> = [];
 
-	constructor(type: LocalizableFieldType, name: string) {
+	constructor(type: LocalizableFieldType, name: string, disabledLanguages: Array<string>) {
 		let thisObj = this;
 		this.name = name;
 		this.$target = jQuery('<div class="localizable-text">');
 		// create a language selector
-		this.languageSelector = new LanguageSelectorWidget(name + '.locale');
+		this.languageSelector = new LanguageSelectorWidget(name + '.locale', disabledLanguages, navigator.language);
 		this.languageSelector.$target.addClass('locale').appendTo(this.$target);
+		this.languageSelector.$target.change(function() {
+			thisObj.languageSelectionChangedListener.forEach(listener => { listener.changed(); });
+		});
 		// create an input or textarea
 		if (type === LocalizableFieldType.Input) {
 			this.$target.append(jQuery('<input type="text" class="text"/>').attr('name', name + '.text'));
@@ -85,6 +111,10 @@ export class LocalizableTextWidget {
 			});
 	}
 
+	public addLanguageSelectionChangedListener(listener: LanguageSelectionChangedListener): void {
+		this.languageSelectionChangedListener.push(listener);
+	}
+
 	public addWidgetDestroyListener(listener: WidgetDestroyedListener<LocalizableTextWidget>): void {
 		this.destroyListeners.push(listener);
 	}
@@ -96,9 +126,17 @@ export class LocalizableTextWidget {
 	public text(): string {
 		return this.$target.children('.text').val();
 	}
+
+	public updateDisabledLanguages(disabledLanguages: Array<string>) {
+		this.languageSelector.updateDisabledLanguages(disabledLanguages);
+	}
 }
 
 export enum LocalizableFieldType {
 	Input,
 	Textarea
+}
+
+interface LanguageSelectionChangedListener {
+	changed(): void;
 }
