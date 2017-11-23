9308a56e2c6dee3d0be78a934f8ee31379a3d5a5
Ralf Hergert <ralf.hergert@gmx.de>
2017-11-23 13:02:16 +0100

    added OpenLayers to show a map

---
+++ backend/src/main/ts/view/AddressMapView.ts
@@ -0,0 +1,62 @@
+import {EntityListener} from '../controller/RequestController';
+import {Address} from '../model/Address';
+
+/**
+ * This view renders the given address on a map.
+ */
+export class AddressMapView implements EntityListener<Address> {
+	private $target: any; // should be a jQuery node.
+	private map: any;
+
+	constructor($target: any) {
+		this.$target = $target;
+		this.map = new ol.Map({
+			target: $target.attr('id'),
+			layers: [
+				new ol.layer.Tile({
+					source: new ol.source.OSM()
+				})
+			],
+			controls: ol.control.defaults({
+				attributionOptions: /** @type {olx.control.AttributionOptions} */ ({
+					collapsible: false
+				})
+			}),
+			view: new ol.View({
+				center: ol.proj.fromLonLat([0, 0]),
+				zoom: 4
+			})
+		});
+		// modify all links Map created to use a blank target.
+		this.map.once('postrender', function() {
+			this.find('a')
+				.attr('target', '_blank')
+				.attr('tabIndex', '-1'); // remove these link from the tab order
+			this.find('button').attr('tabIndex', '-1'); // remove these buttons from the tab order
+		}, this.$target);
+	}
+
+	public updateEntity(address: Address) {
+		if (address != null && address.lon != null && address.lat != null) {
+			this.flyTo(address.lon, address.lat);
+		}
+	}
+
+	public flyTo(lon: number, lat: number) {
+		let duration = 2000;
+		// animation of the translational change
+		this.map.getView().animate({
+			center: ol.proj.fromLonLat([lon, lat]),
+			duration: duration
+		});
+		// animation of the zoom.
+		var zoom = this.map.getView().getZoom();
+		this.map.getView().animate({
+			zoom: zoom - 1,
+			duration: duration / 2
+		}, {
+			zoom: zoom,
+			duration: duration / 2
+		});
+	}
+}
