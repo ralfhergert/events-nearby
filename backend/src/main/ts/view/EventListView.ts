1c724710c09622d8013c61b5bdae7bc9a53e3e42
Ralf Hergert <ralf.hergert@gmx.de>
2017-11-29 12:22:18 +0100

    fixed rendering of event items

--- backend/src/main/ts/view/EventListView.ts
+++ backend/src/main/ts/view/EventListView.ts
@@ -18,22 +18,22 @@ export class EventListView implements EntityListener<Array<LocalEvent>> {
 		// remove all events which are no longer in the given list.
 		this.$target.children('.local-event-view').each(function() {
 			if (existingLocalEventIds.indexOf(jQuery(this).attr('data-local-event-id')) < 0) {
-				jQuery(this).fadeOut(function() { jQuery(this).remove(); });
+				jQuery(this).slideUp(function() { jQuery(this).remove(); });
 			}
 		});
 		// add new events is they do not exist in the rendered view yet.
-		let $marker = jQuery('<div class="marker">').prepend(this.$target);
+		let $marker = jQuery('<div class="marker">').prependTo(this.$target);
 		let $previousNode = $marker;
 		for (let event of events) {
-			let $node = this.$target.find('#' + event.id);
+			let $node = this.$target.find('[data-local-event-id="' + event.id + '"]');
 			if ($node.length > 0) { // event does exist.
 				$previousNode = $node;
 			} else { // event does not exist - render it
 				$previousNode = jQuery('<div class="local-event-view">')
 					.attr('data-local-event-id', event.id)
 					.append(jQuery('<span class="title">').text(event.title['de']))
-					.append(jQuery('<span class="description">').text(event.description.get['de']))
-					.fadeIn()
+					.append(jQuery('<span class="description">').text(event.description['de']))
+					.slideDown()
 					.insertAfter($previousNode);
 			}
 		}
